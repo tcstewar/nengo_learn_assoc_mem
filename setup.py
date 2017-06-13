1204f2db74b1052dbd4c9dfcc668bdc868927bfd
Terry Stewart <tcstewar@uwaterloo.ca>
2017-06-13 15:03:24 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nengo_learn_assoc_mem',
+    packages=['nengo_learn_assoc_mem'],
+    version='0.0.1',
+    author='Terry Stewart',
+    description='Nengo model of learning an associative memory',
+    author_email='terry.stewart@gmail.com',
+    url='https://github.com/tcstewar/nengo_learn_assoc_mem',
+    license='MIT',
+    )
