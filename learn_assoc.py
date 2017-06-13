f900de73d4b584804dedb235e15a797a974792eb
Terry Stewart <tcstewar@uwaterloo.ca>
2017-06-12 22:36:49 -0400

    Added training ability

---
+++ learn_assoc.py
@@ -0,0 +1,64 @@
+import nengo
+import nengo.spa as spa
+import numpy as np
+
+
+class LearningAssocMem(nengo.Network):
+    def __init__(self, n_neurons, dimensions,
+                 intercepts=nengo.dists.Uniform(0,0),
+                 voja_tau=0.005,
+                 voja_rate=1e-3,
+                 pes_rate=1e-3,
+                 label=None,
+                 inhibit_synapse=0.01,
+                 inhibit_strength=0.0005,
+                 load_from=None
+                 ):
+        super(LearningAssocMem, self).__init__(label=label)
+        if load_from is not None:
+            data = np.load(load_from)
+            encoders = data['enc']
+            decoders = data['dec']
+        else:
+            encoders = nengo.Default
+            decoders = np.zeros((dimensions, n_neurons), dtype=float)
+        with self:
+            self.mem = nengo.Ensemble(n_neurons=n_neurons,
+                                      dimensions=dimensions,
+                                      intercepts=intercepts,
+                                      encoders=encoders,
+                                      )
+            self.input = nengo.Node(None, size_in=dimensions)
+            self.output = nengo.Node(None, size_in=dimensions)
+            self.correct = nengo.Node(None, size_in=dimensions)
+
+            nengo.Connection(self.input, self.mem,
+                learning_rule_type=nengo.Voja(post_tau=voja_tau,
+                                              learning_rate=voja_rate))
+
+
+            self.conn_out = nengo.Connection(self.mem.neurons, self.output,
+                transform=decoders,
+                learning_rule_type=nengo.PES(learning_rate=pes_rate),
+                )
+
+            nengo.Connection(self.output, self.conn_out.learning_rule)
+            nengo.Connection(self.correct, self.conn_out.learning_rule,
+                             transform=-1)
+
+            nengo.Connection(self.mem.neurons, self.mem.neurons,
+                transform=inhibit_strength*(np.eye(self.mem.n_neurons)-1),
+                synapse=inhibit_synapse)
+
+    def create_weight_probes(self):
+        with self:
+            self.probe_encoder = nengo.Probe(self.mem, 'scaled_encoders',
+                                             sample_every=0.1)
+            self.probe_decoder = nengo.Probe(self.conn_out, 'weights',
+                                             sample_every=0.1)
+
+    def save(self, filename, sim):
+        enc = sim.data[self.probe_encoder][-1]
+        dec = sim.data[self.probe_decoder][-1]
+        np.savez(filename, enc=enc, dec=dec)
+
---
+++ learn_trial.py
@@ -0,0 +1,108 @@
+import pytry
+import nengo
+import nengo.spa as spa
+import numpy as np
+import random
+
+import sys
+if '.' not in sys.path:
+    sys.path.append('.')
+import learn_assoc
+
+class LearningAssocMemTrial(pytry.NengoTrial):
+    def params(self):
+        self.param('number of neurons', n_neurons=10)
+        self.param('number of dimensions', dimensions=32)
+        self.param('intercept', intercept=0.0)
+        self.param('VOja time constance', voja_tau=0.005)
+        self.param('VOja learning rate', voja_rate=1e-3)
+        self.param('PES learning rate', pes_rate=1e-3)
+        self.param('WTA synapse', inhibit_synapse=0.01)
+        self.param('WTA strength', inhibit_strength=0.0005)
+        self.param('presentation time', t_present=0.1)
+        self.param('number of items', n_items=6)
+        self.param('number of presentations per item', n_present=4)
+        self.param('lower scale on input values', input_scale=0.5)
+        self.param('filename for saving/loading', filename='weights.npz')
+        self.param('load weights at beginning', load=False)
+        self.param('save weights at end', save=False)
+
+    def model(self, p):
+        self.items = list(range(p.n_items))*p.n_present
+        random.shuffle(self.items)
+
+        vocab_items = ['X%d' % i for i in self.items]
+        vocab = spa.Vocabulary(p.dimensions)
+
+        model = nengo.Network()
+        with model:
+            def stim_func(t):
+                index = int(t / p.t_present)
+                scale = 1.0
+                if index > len(vocab_items)/2:
+                    scale = p.input_scale
+                index = index % len(vocab_items)
+                return vocab.parse('%g * %s' % (scale, vocab_items[index])).v
+            stim = nengo.Node(stim_func)
+
+            def correct_func(t):
+                index = int(t / p.t_present)
+                scale = 1.0
+                index = index % len(vocab_items)
+                return vocab.parse('%g * %s' % (scale, vocab_items[index])).v
+            correct = nengo.Node(correct_func)
+
+            self.mem = learn_assoc.LearningAssocMem(
+                    n_neurons=p.n_neurons,
+                    dimensions=p.dimensions,
+                    intercepts=nengo.dists.Uniform(p.intercept, p.intercept),
+                    voja_tau=p.voja_tau,
+                    voja_rate=p.voja_rate,
+                    pes_rate=p.pes_rate,
+                    inhibit_synapse=p.inhibit_synapse,
+                    inhibit_strength=p.inhibit_strength,
+                    load_from = None if not p.load else p.filename,
+                    )
+            if p.save:
+                self.mem.create_weight_probes()
+            nengo.Connection(stim, self.mem.input, synapse=None)
+            nengo.Connection(correct, self.mem.correct, synapse=None)
+
+            self.p_output = nengo.Probe(self.mem.output, synapse=0.01)
+            self.p_ideal = nengo.Probe(correct, synapse=0.01)
+
+        return model
+
+    def evaluate(self, p, sim, plt):
+        sim.run(p.n_present*p.n_items*p.t_present)
+        if p.save:
+            self.mem.save(p.filename, sim)
+
+        data = sim.data[self.p_output]
+        ideal = sim.data[self.p_ideal]
+        accuracy = np.sum(data * ideal, axis=1)
+
+        times = np.arange(p.n_items*p.n_present)*p.t_present
+        indices = (times / p.dt).astype(int) - 1
+
+        scores = [[] for i in range(p.n_items)]
+
+        for i, index in enumerate(indices):
+            a = accuracy[index]
+            item = self.items[i]
+            scores[item].append(a)
+
+        scores = np.array(scores)
+
+        if plt:
+            plt.plot(scores.T, alpha=0.3)
+            plt.plot(np.mean(scores, axis=0))
+
+
+
+
+
+
+
+
+
